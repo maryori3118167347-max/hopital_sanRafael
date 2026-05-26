6b3bed0dd0d03ed594a28d326b937180a2a458f9
maryori3118167347-max <maryori3118167347@gmail.com>
2026-05-26 18:59:08 -0500

    Delete auth_service_san_Rafael directory

--- auth_service_san_Rafael/models/userModel.py
+++
@@ -1,8 +0,0 @@
-from extensions import db
-
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
