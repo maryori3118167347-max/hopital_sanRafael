6da321146db6487d4ac7ba69cfd5db1e2ceb80b5
maryori3118167347-max <maryori3118167347@gmail.com>
2026-05-26 18:59:35 -0500

    Delete locations_service directory

--- locations_service/config.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = "super_secret_key"
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:0000@localhost:5432/microservices_hospital_san_rafael2_db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
